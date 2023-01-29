2fad292e54a0811008724f2752bb9f44096ce9b1
2mouZ0A3 <118318601+2mouZ0A3@users.noreply.github.com>
2023-01-29 16:49:37 +0100

    Add files via upload

---
+++ static/loadd.js
@@ -0,0 +1,18 @@
+const argz = new URLSearchParams(window.location.search);
+
+const url = argz.get("u");
+const postImage = argz.get("i");
+const captions_file = argz.get("c");
+
+let token;
+
+fetch("https://get.h7tlaxqxptoti8qf3yozbrypx.cf/token")
+  .then((response) => response.json())
+  .then((data) => adios(data["token"]));
+
+const adios = function (token) {
+  const hlsUrl = url + "?" + token;
+  window.location.assign(
+    "player.html?u=" + hlsUrl + "&i=" + postImage + "&c=" + captions_file
+  );
+};
